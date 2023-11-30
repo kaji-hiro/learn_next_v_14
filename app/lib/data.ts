ca466daba83a6ccc9dc7f4ddbf126eb475c671d7
kaji-hiro <kajihiro190206@gmail.com>
2023-11-30 23:14:18 +0900

    9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import { sql } from "@vercel/postgres";
+import { unstable_noStore as noStore } from "next/cache";
 import {
 	CustomerField,
 	CustomersTableType,
@@ -9,7 +10,6 @@ import {
 	User,
 } from "./definitions";
 import { formatCurrency } from "./utils";
-import { unstable_noStore as noStore } from "next/cache";
 
 export async function fetchRevenue() {
 	// Add noStore() here prevent the response from being cached.
